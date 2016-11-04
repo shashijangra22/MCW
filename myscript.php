c712e5cb68662b2a3c0f90c8a79bd4f37e0f0898
Shashi Jangra <shashijangra@live.com>
2016-11-05 01:59:32 +0530

    final script lol :P

--- myscript.php
+++ myscript.php
@@ -1,3 +1,5 @@
+<?php
+
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
 $connectstr_dbusername = '';
@@ -27,3 +29,5 @@ echo "Success: A proper connection to MySQL was made! The my_db database is grea
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
 mysqli_close($link);
+
+?>
