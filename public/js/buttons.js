4225ad1a4b84620dd99123c0a9c2086ee61124cc
Shashi Jangra <shashijangra@live.com>
2016-11-19 05:09:13 +0530

    Enter enabled comments on confessions

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -128,6 +128,11 @@ $('.comment_input').keypress(function (e) {
 
  $(".commentscount").on("click",function(){
 	var pid=$(this).data('id');
+	if ($('#'+pid+'commentbox').is(':visible')) 
+	{
+		$('#'+pid+'commentbox').hide('normal');
+		return 0;
+	}
 	$.ajax({
 	type:'POST',
 	url:'showcomments',
@@ -141,6 +146,7 @@ $('.comment_input').keypress(function (e) {
 		}
 		else
 		{
+			$('#'+pid+'likesbox').hide('normal');
 			for(var key in result)
 			{
 				$('#'+pid+'commentbox').append('<div class="row" style="padding-top: 5px;font-size: 12px;margin:auto"><img src="'+result[key].displaypic+'" class="circle profile-pic" width="12" height="12" />	<b>'+result[key].username+'</b> '+result[key].data+'</div>');	
@@ -158,6 +164,12 @@ $('.comment_input').keypress(function (e) {
 
  $(".likescount").on("click",function(){
 	var pid=$(this).data('id');
+	if ($('#'+pid+'likesbox').is(':visible')) 
+	{
+		$('#'+pid+'likesbox').hide('normal');
+		return 0;
+	}
+
 	$.ajax({
 	type:'POST',
 	url:'showlikes',
@@ -171,11 +183,12 @@ $('.comment_input').keypress(function (e) {
 		}
 		else
 		{
+			$('#'+pid+'commentbox').hide('normal');
 			for(var key in result)
 			{
-				$('#'+pid+'likesbox').append('<div class="row" style="padding-top: 5px;font-size: 12px;margin:auto"><img src="'+result[key].displaypic+'" class="circle profile-pic" width="12" height="12" />	<b>'+result[key].username+'</b></div>');	
+				$('#'+pid+'likesbox').append('<p style="font-size:12px;display:inline"><b> '+result[key].username+',</b></p>');	
 			}
-			$('#'+pid+'commentbox').show('normal');
+			$('#'+pid+'likesbox').show('normal');
 
 		}
 });
