3e941c9f5cb560fa01902a38c984c27d4d91604a
Shashi Jangra <shashijangra@live.com>
2016-11-19 21:02:08 +0530

    in progress...

--- resources/views/societies.blade.php
+++ resources/views/societies.blade.php
@@ -8,13 +8,13 @@
 		    <div class="col s12">
 		      <ul class="tabs">
 		        <li class="tab col s3 active"><a href="#test1">Dance</a></li>
-		        <li class="tab col s3"><a href="#test2">Drama</a></li>
-		        <li class="tab col s3"><a href="#test3">Music</a></li>
-		        <li class="tab col s3"><a href="#test4">Misc</a></li>
+		        <li class="tab col s3 disabled"><a href="#test2">Drama</a></li>
+		        <li class="tab col s3 disabled"><a href="#test3">Music</a></li>
+		        <li class="tab col s3 disabled"><a href="#test4">Misc</a></li>
 		      </ul>
 		    </div>
 		    <div id="test1" class="col s12">
-		    	<div class="row">
+		    	<div class="row" style="margin-bottom: 0px">
 		    		<div class="col s12">
 		            <div class="card">
 		                <div class="card-image waves-effect waves-block waves-light">
@@ -24,13 +24,26 @@
 		    		</div>
 		    	</div>
 	            <div class="row">
+	            	<div class="col l4">
+	            		<div class="card">
+		                  <div class="card-content" style="padding-top: 10px;padding-bottom: 10px">
+		                    <span class="card-title">
+		                    About Us
+			                    <a class="right" href="https://www.facebook.com/pg/rage.bcas"><i class="fa fa-facebook"></i></a>
+					            <a style="padding-right: 10px" class="right" href="https://www.instagram.com/rage_western_dance_soc_bcas/"><i class="fa fa-instagram"></i></a>
+				            </span>
+		                    <blockquote style="text-align: justify;margin-top: 10px;margin-bottom:10px ">Great dancers are not great because of their technique, they are great because of their passion. 
+just GET UP AND DANCE :D</blockquote>
+		                  </div>                
+		                </div>
+	            	</div>
 	            	<div class="col s12 m12 l8">
 	            		<div class="row">
 						    <div class="col s12">
 						      <ul class="tabs">
-						        <li class="tab col s4 active"><a href="#dancePosts">Posts</a></li>
-						        <li class="tab col s4"><a href="#dancePhotos">Photos</a></li>
-						        <li class="tab col s4"><a href="#danceVideos">Videos</a></li>
+						        <li class="tab col s4 disabled"><a href="#dancePosts">Posts</a></li>
+						        <li class="tab col s4 disabled"><a href="#dancePhotos">Photos</a></li>
+						        <li class="tab col s4 active"><a href="#danceVideos">Videos</a></li>
 						      </ul>
 						    </div>
 						    <div id="dancePosts" class="col s12">
@@ -53,26 +66,22 @@
 				                @endfor
 						    </div>
 						    <div id="danceVideos" class="col s12">
-						    	@for ($i=0; $i < 5; $i++)
 						    	<div class="card">
 				                  <div class="card-image">
 				                  	<div class="video-container no-controls">
-			                            <iframe src="https://www.youtube.com/embed/10r9ozshGVE" frameborder="0" allowfullscreen></iframe>
-				                  	<!-- <img src="http://demo.geekslabs.com/materialize/v2.1/layout03/images/user-profile-bg.jpg"> -->
-			                         </div>
+			                            <iframe src="https://www.youtube.com/embed/Q2NK6Vmv6vo" frameborder="0" allowfullscreen></iframe>
+			                        </div>
 				                  </div> 
 				                </div>
-				                @endfor
+				                <div class="card">
+				                  <div class="card-image">
+				                  	<div class="video-container no-controls">
+			                            <iframe src="https://www.youtube.com/embed/aHJ7dfatyg0" frameborder="0" allowfullscreen></iframe>
+			                        </div>
+				                  </div> 
+				                </div>
 						    </div>
 						  </div>
-	            	</div>
-	            	<div class="col l4">
-	            		<div class="card light-blue">
-		                  <div class="card-content white-text">
-		                    <span class="card-title">About Me!</span>
-		                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-		                  </div>                
-		                </div>	
 	            	</div>
 	            </div>
 		    </div>
