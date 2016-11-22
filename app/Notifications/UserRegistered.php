9c4f9f706807372b983bafcc11d7462355d09842
Shashi Jangra <shashijangra@live.com>
2016-11-22 22:21:14 +0530

    New Mail Notification at registration of user

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class UserRegistered extends Notification
+{
+
+    use Queueable;
+
+    protected $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user=$user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->success()
+                    ->from('admin@mycollegewall.com','MCW')
+                    ->subject('Welcome to MCW!')
+                    ->line('Congratulations ' . $this->user->fname . ' :) You have been successfully registered!')
+                    ->action('Activate Account', url('/verify/' . $this->user->verifytoken))
+                    ->line('Make sure to Activate your account before logging in :)');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
