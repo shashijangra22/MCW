68bcb967e20c1f7c549fa959e1d342cd2fc8d88f
Shashi Jangra <shashijangra@live.com>
2016-11-22 22:15:08 +0530

    Fillable fields updated

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'fname', 'verifytoken', 'active', 'lname', 'username', 'email', 'password',
     ];
 
     /**
